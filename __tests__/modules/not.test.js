bb1d68d68c4e9114c403a5405e3f1b167b8b0618
troberts <timiroberts@gmail.com>
2018-01-26 08:16:42 -0600

    Add Not

---
+++ __tests__/modules/not.test.js
@@ -0,0 +1,15 @@
+import not from '../../modules/not'
+
+describe('Not', () => {
+  it('returns true if the passed in function returns falsey', () => {
+    const fn = () => 0
+    const notFn = not(fn)
+    expect(notFn()).toBe(true)
+  })
+
+  it('returns false if the passed in function returns truthy', () => {
+    const fn = () => 1
+    const notFn = not(fn)
+    expect(notFn()).toBe(false)
+  })
+})
---
+++ modules/not.js
@@ -0,0 +1,3 @@
+const not = fn => (...args) => !fn(...args)
+
+export default not
